e7e34013c974294cf8380c76bc85004f9bd3b422
G3UKB <bob@bobcowdery.plus.com>
2017-06-05 22:07:06 +0100

    Developing.

--- python/test.py
+++ python/test.py
@@ -5,4 +5,7 @@ import socket
 import pickle
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('127.0.0.1', 10003))
 sock.sendto(pickle.dumps(['fres', 1800, 1850]), ('127.0.0.1', 10002))
+data, address = sock.recvfrom(1000)
+print(pickle.loads(data))
