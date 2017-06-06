025a51c25bddc4845236c2d2c0ba4cc7bfe1a737
G3UKB <bob@bobcowdery.plus.com>
2017-06-06 16:54:04 +0100

    First cut operational.

--- python/test.py
+++ python/test.py
@@ -6,7 +6,7 @@ import pickle
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('127.0.0.1', 10003))
-#sock.sendto(pickle.dumps(['fres', 1800000, 1850000]), ('127.0.0.1', 10002))
+#sock.sendto(pickle.dumps(['fres', 1800000, 3000000]), ('127.0.0.1', 10002))
 #sock.sendto(pickle.dumps(['fswr', 1800000]), ('127.0.0.1', 10002))
 sock.sendto(pickle.dumps(['scan', 1800000, 1850000]), ('127.0.0.1', 10002))
 data, address = sock.recvfrom(1000)
