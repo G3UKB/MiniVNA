4a0fa99dfdb53f57841d255045a33809edefbda5
Bob Cowdery <bob@bobcowdery.plus.com>
2019-06-15 15:54:12 +0100

    Re-testing.

--- python/test.py
+++ python/test.py
@@ -6,7 +6,7 @@ import pickle
 
 CMD_PORT = 10002
 RESPONSE_PORT = 10003
-ARDUINO_IP = '192.168.1.108'
+SERVER_IP = '192.168.1.112'
 
 # Create a datagram socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,6 +22,6 @@ print('Resonant at - ', pickle.loads(data))
 
 # Query the SWR at the given frequency
 f = 1837000
-sock.sendto(pickle.dumps(['fswr', f]), (ARDUINO_IP, CMD_PORT))
+sock.sendto(pickle.dumps(['fswr', f]), (SERVER_IP, CMD_PORT))
 data, address = sock.recvfrom(1000)
 print('SWR at %d - ' % (f), pickle.loads(data))
